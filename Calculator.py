29c8c03ed6ef803a51a67f8194e068cda0e8adca
Jay Shinde <jshinde@Jays-MBP.attlocal.net>
2022-10-10 20:46:30 -0700

    refactored code and documentation strings

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,23 @@
 """
-This Program takes returns a plan to divvy up income into buckets pre-set by the user.
-Input: Pre-Tax Yearly Income
-Output: Income Plan
+This Python script uses the 50/30/20 rule in Financial Budgeting to help give the user rough guidance as to what to budget and how much.
+Author: Jay Shinde 
 
-Author: Jay Shinde 2022
+Tax Data Source: https://taxfoundation.org/2022-tax-brackets/ 
+50/30/20 Rule Source: https://n26.com/en-eu/blog/50-30-20-rule
 """
 class Calc:
+    """
+    Main Calculator Class 
+    """
 
     #Helper Functions
 
-    """
-    Input: dirty string containing chars that cannot be casted to type float
-    Output: float type representing a cleaned version of string
-    """
-    def cleanInput(dollarAmount):
+
+    def cleanInput(dollarAmount: str) -> (float | None):
+        """
+        Input: dirty string that may contain non-numerical chars
+        Output: float representation of cleaned dirty string, none if non-castable
+        """
         dollarAmount = dollarAmount.replace("$", "")
         dollarAmount = dollarAmount.replace(",", "")
         if dollarAmount.isdigit():
@@ -21,19 +25,21 @@ class Calc:
         else:
             res = None
         return res
-    """
-    Input: float representation for dollar amount
-    Output: formatted string with $ and ,
-    """
-    def formatDollar(dollarFloat):
+    
+
+    def formatDollar(dollarFloat: float) -> str:
+        """
+        Input: float representation for dollar amount
+        Output: formatted string with $ and ,
+        """
         return "${:,.2f}".format(dollarFloat)
 
-    """
-    Input: Income of the user of type float
-    Output: tax percentage of the income by bracket of type int
-    Tax Data Source: https://taxfoundation.org/2022-tax-brackets/
-    """
-    def getTaxAmount(preTaxIncome):
+
+    def getTaxAmount(preTaxIncome: float) -> int:
+        """
+        Input: income of the user
+        Output: tax percentage of the income 
+        """
         if preTaxIncome > 0:
             taxPercentage = 10
             if preTaxIncome > 10275:
@@ -50,55 +56,60 @@ class Calc:
                                     taxPercentage = 37
         return taxPercentage
 
-    """
-    Input: Yearly Income of type float
-    Output: Monthly Income of type float
-    """
-    def getMonthlyFromYearly(yearlyIncome):
+
+    def getMonthlyFromYearly(yearlyIncome: float) -> float:
+        """
+        Input: Yearly Income
+        Output: Monthly Income
+        """
         return yearlyIncome / 12
 
-    """
-    Input: Yearly Income of type float
-    Output: Hourly Income of type float
-    """
-    def getHourlyFromYearly(yearlyIncome):
+
+    def getHourlyFromYearly(yearlyIncome: float) -> float:
+        """
+        Input: Yearly Income
+        Output: Hourly Income
+        """
         return yearlyIncome / 40 / 52
 
-    """
-    Input: Monthly Income of type float
-    Output: Yearly Income of type float
-    """
-    def getYearlyFromMonthly(monthlyIncome):
+
+    def getYearlyFromMonthly(monthlyIncome: float) -> float:
+        """
+        Input: Monthly Income
+        Output: Yearly Income
+        """
         return monthlyIncome * 12
 
-    """
-    Input: Monthly Income of type float
-    Output: Hourly Income of type float
-    """
-    def getHourlyFromMonthly(monthlyIncome):
+
+    def getHourlyFromMonthly(monthlyIncome: float) -> float:
+        """
+        Input: Monthly Income
+        Output: Hourly Income
+        """
         return monthlyIncome * 12 / 40 / 52
 
-    """
-    Input: Hourly Income of type float
-    Output: Yearly Income of type float
-    """
-    def getYearlyFromHourly(hourlyIncome):
+
+    def getYearlyFromHourly(hourlyIncome: float) -> float:
+        """
+        Input: Hourly Income
+        Output: Yearly Income
+        """
         return hourlyIncome * 40 * 52
 
-    """
-    Input: Monthly Income of type float
-    Output: Yearly Income of type float
-    """
-    def getMonthlyFromHourly(hourlyIncome):
+
+    def getMonthlyFromHourly(hourlyIncome: float) -> float:
+        """
+        Input: Monthly Income
+        Output: Yearly Income
+        """
         return hourlyIncome * 40 * 52 / 12
     
+
     #Initialize variables
     preTaxIncome = inputIncome = taxPercentage = incomeType = incomeMinus401k = 0
-    
     hourlyWage = None
 
     #Let user choose what income to use for the calculation.
-
     print("================================================")
     print("Welcome, use this tool to give you a rough idea of how you should be spending your paychecks.")
     print("================================================\n")
@@ -115,25 +126,24 @@ class Calc:
         elif incomeSelect == "3":
             incomeType = 3
         else:
-            print("Please pick a valid option.\n")
+            print("Please enter a valid option.\n")
 
     #Prompt user for their income
-
-    if incomeType == 1: #Hourly
+    if incomeType == 1:             #Hourly
         hourlyIncome = cleanInput(input("What is your Hourly Income? Enter: "))
         while hourlyIncome == None: 
             print("Sorry, that's not a valid income. Try Again.")
             hourlyIncome = cleanInput(input("What is your Hourly Income? Enter: "))
         preTaxIncome = inputIncome = getYearlyFromHourly(hourlyIncome)
         monthlyIncome = getMonthlyFromHourly(hourlyIncome)
-    elif incomeType == 2: #Monthly
+    elif incomeType == 2:           #Monthly
         monthlyIncome = cleanInput(input("What is your Monthly Income? Enter: "))
         while monthlyIncome == None:
             print("Sorry, that's not a valid income. Try Again.")
             monthlyIncome = cleanInput(input("What is your Monthly Income? Enter: "))
         preTaxIncome = inputIncome = getYearlyFromMonthly(monthlyIncome)
         hourlyIncome = getHourlyFromMonthly(monthlyIncome)
-    elif incomeType == 3: #Annually
+    elif incomeType == 3:           #Annually
         preTaxIncome = inputIncome = cleanInput(input("What is your Annual Income? Enter: "))
         while preTaxIncome == None:
             print("Sorry, that's not a valid income. Try Again.")
@@ -141,45 +151,33 @@ class Calc:
         hourlyIncome = getHourlyFromYearly(preTaxIncome)
         monthlyIncome = getMonthlyFromYearly(preTaxIncome)
 
-    # Does the user have a 401k? If so calculate contribution
-
-    user401k = input("Do you have a 401(k) Account? Enter Y or N : ")
-    if user401k == "Y":
-        percentMatch401kSalary = float(input("What percent does your company maximally match of your salary? Example: 6 Percent = 6. Enter : "))
-        percentMatch401kCompany = float(input("What percent does your company maximally match of your contribution? Example: 100 Percent = 100. Enter : "))
+    # Does the user have a 401k? If so calculate contribution and deduct from pre-tax income
+    user401k = input("Do you have a 401(k) Account? Enter Y or N : ").lower()
+    if user401k == "y" or user401k == "yes":
+        percentMatch401kSalary = float(input("What percent does your company maximally match your salary? Example: 6 Percent = 6. Enter : "))
+        percentMatch401kCompany = float(input("What percent does your company maximally match your contribution? Example: 100 Percent = 100. Enter : "))
         companyMatchingPercentage = percentMatch401kCompany/100
         user401kContribution = company401kContribution = preTaxIncome * (percentMatch401kSalary/100)
         company401kContribution *= companyMatchingPercentage
-
-        #calculate amount to go into 401k yearly
-        yearly401kIn = user401kContribution + company401kContribution  
-
-        #update income
-        preTaxIncome -= yearly401kIn     
+        yearly401kIn = user401kContribution + company401kContribution  #calculate amount to go into 401k yearly
+        preTaxIncome -= yearly401kIn                                   #update income
     else:
         yearly401kIn = 0
-
 
     #Calculate Post-Tax Income 
     incomeMinus401k = preTaxIncome  
     taxPercentage = getTaxAmount(preTaxIncome)
     postTaxIncome = preTaxIncome - (preTaxIncome * taxPercentage / 100)
-    postTaxMonthlyIncome = postTaxIncome / 12
 
     # Use 50/20/30 Rule for Income Split
+    yearlyNeeds = postTaxIncome * 0.5                       # 50% : Needs
+    monthlyNeeds = getMonthlyFromYearly(yearlyNeeds)        #
+    yearlyWants = postTaxIncome * 0.3                       # 30% : Wants
+    monthlyWants = getMonthlyFromYearly(yearlyWants)        #
+    yearlySavings = postTaxIncome * 0.2                     # 20% : Savings
+    monthlySavings = getMonthlyFromYearly(yearlySavings)    #
 
-    # 50% : Needs
-    yearlyNeeds = postTaxIncome * 0.5
-    monthlyNeeds = postTaxMonthlyIncome * 0.5
-    # 30% : Wants
-    yearlyWants = postTaxIncome * 0.3
-    monthlyWants = postTaxMonthlyIncome * 0.3
-    # 20% : Savings
-    yearlySavings = postTaxIncome * 0.2
-    monthlySavings = postTaxMonthlyIncome * 0.2
-
-
-    #print results
+    #print results in format
     print(
         "========================RESULTS========================\n",
         "Annual Income : ",formatDollar(inputIncome),"\n",
@@ -193,7 +191,7 @@ class Calc:
         "Annual Income after Tax and 401(k) deductions : ",formatDollar(postTaxIncome),"\n",
         "Monthly Income after Tax and 401(k) deductions : ",formatDollar(getMonthlyFromYearly(postTaxIncome)),"\n",
         "Hourly Income after Tax and 401(k) deductions : ",formatDollar(getHourlyFromYearly(postTaxIncome)),"\n\n",
-        "Let's calculate how you should be spending your money according to the 50/20/30 Rule\n\n",
+        "Let's calculate how you should be spending your money according to the 50/30/20 Rule\n\n",
         "'NEEDS' are those bills that you absolutely must pay and are the things necessary for survival. Examples are Rent, Utilities, Car payments, Groceries, and more.\n",
         "Your 'needs' category has a annual budget of ",formatDollar(yearlyNeeds), " or ",formatDollar(monthlyNeeds), " monthly.\n\n",
         "'WANTS' Wants are all the things you spend money on that are not absolutely essential. Examples are Dinner Out, Weekends out, Concerts, and more.\n",
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Budgeting-Calculator
+
+I made this script to help me and possibly you to budget your finances a little better using the 50/30/20 Rule. This tool uses 2022 Tax Information and includes factors in calculation of any 401(k) contributions the user may have in providing the resulting budget allowances.
+
+Sources:
+https://n26.com/en-eu/blog/50-30-20-rule 
+https://taxfoundation.org/2022-tax-brackets/ 
